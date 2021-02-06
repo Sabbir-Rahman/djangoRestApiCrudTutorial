7ab0e88f7eef13b725b13d835d46dab5f4b5374b
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-06 10:00:25 +0600

    delete

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('task-list/',views.taskList, name="task-list"),
     path('task-details/<str:pk>/',views.taskListDetails, name="task-detail"),
     path('task-update/<str:pk>/',views.taskUpdate, name="task-update"),
+    path('task-delete/<str:pk>/',views.taskDelete, name="task-delete"),
     path('task-create',views.taskCreate, name="task-create"),
 ]
